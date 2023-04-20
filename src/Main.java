609ef96d3475a56ba23f0db8611b3a0c77fa8f71
belova <belova@geobuilder.ru>
2023-04-20 16:24:36 +0300

    settings and properties

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import db.DBConnector;
+import db.IDBConnector;
+
 public class Main {
 
     public static void main (String[] args){
-
+        IDBConnector idbConnector = new DBConnector();
+        idbConnector.execeteRequest("create table student columns (id int primary key, name varchar(500));");
     }
 
 }

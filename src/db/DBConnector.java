1cfbcf146d062f6911327cf1511823391189b7ec
belova <belova@geobuilder.ru>
2023-04-20 12:35:48 +0300

    settings and properties

--- src/db/DBConnector.java
+++ src/db/DBConnector.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class DBConnector implements IDBConnector{
 
     private static Connection connection = null;
+    private static Statement statement = null;
 
     public DBConnector() {
         connect();
@@ -14,13 +15,20 @@ public class DBConnector implements IDBConnector{
         if (connection == null) {
             connection = DriverManager.getConnection();
         }
+
+        if (statement == null) {
+            try {
+                connection.createStatement();
+            } catch(SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     @Override
     public ResultSet execete(String request)  {
         try {
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(request);
+            return statement.executeQuery(request);
         } catch(SQLException ex) {
             ex.printStackTrace();
         }
@@ -30,11 +38,29 @@ public class DBConnector implements IDBConnector{
 
     @Override
     public void execeteRequest(String request) {
+        try {
+            statement.execute(request);
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }
 
     }
 
     @Override
     public void close() {
-
+        if (statement != null){
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (connection != null) {
+               try {
+                   connection.close();
+               } catch (SQLException ex) {
+                   ex.printStackTrace();
+               }
+           }
     }
 }
